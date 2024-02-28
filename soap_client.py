7ee67aa6897717300f626549ce3ae1a9ea85fc43
Carlos <jcdosmil2506@gmail.com>
2024-02-28 01:44:36 -0400

    Primer commit

---
+++ soap_client.py
@@ -0,0 +1,10 @@
+from zeep import Client
+client = Client('http://localhost:8000')
+result = client.service.Saludar(nombre="Carlos")
+print (result)
+
+wsdl_url = "https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL"
+client = Client(wsdl_url)
+number_to_convert = 100
+result = client.service.NumberToDollars(number_to_convert)
+print(f"{number_to_convert} en dólares es: {result}")
